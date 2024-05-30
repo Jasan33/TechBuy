09431d727aad6257b9edd43651da29f34d9f7ef5
Jasan33 <156905772+Jasan33@users.noreply.github.com>
2024-05-31 01:30:31 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,89 @@
+function Meny() {
+    let y = document.getElementById("meny");
+    if (y.style.display === "none") {
+      y.style.display = "block";
+    } else {
+      y.style.display = "none";
+    }
+  }
+
+function loginn() {
+    let x = document.getElementById("loginn");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+function Iphone() {
+    let x = document.getElementById("Iphone");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+function X_Iphone() {
+    let x = document.getElementById("Iphone");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+function Gaming_pc() {
+    let x = document.getElementById("Gaming_pc");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+function X_gaming_pc() {
+    let x = document.getElementById("Gaming_pc");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+function TV() {
+    let x = document.getElementById("TV");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+function X_TV() {
+    let x = document.getElementById("TV");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+function Apple_vision() {
+    let x = document.getElementById("Apple_vision");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+function X_Apple_vision() {
+    let x = document.getElementById("Apple_vision");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
